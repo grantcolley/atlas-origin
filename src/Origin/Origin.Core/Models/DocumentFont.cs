c2bd8cc401ef5c6c34c00a3abbb40a9199d29322
Grant Colley <grantcolley@yahoo.com>
2024-09-07 22:00:21 +0100

    Create DocumentFont.cs

---
+++ src/Origin/Origin.Core/Models/DocumentFont.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Origin.Core.Models
+{
+    public class DocumentFont
+    {
+        [Required]
+        [StringLength(150)]
+        public string? Font { get; set; }
+    }
+}
