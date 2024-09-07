c0d9cbfe8639deb81d2c8da8b276b42aac1a1894
Grant Colley <grantcolley@yahoo.com>
2024-09-07 22:20:58 +0100

    Update DocumentFont.cs

--- src/Origin/Origin.Core/Models/DocumentFont.cs
+++ src/Origin/Origin.Core/Models/DocumentFont.cs
@@ -4,6 +4,8 @@ namespace Origin.Core.Models
 {
     public class DocumentFont
     {
+        public int DocumentFontId { get; set; }
+
         [Required]
         [StringLength(150)]
         public string? Font { get; set; }
