10815d58289c8554f5a61b6050f68674691cceba
Grant Colley <grantcolley@yahoo.com>
2024-09-09 20:24:17 +0100

    Update AtlasOriginEndpointMapper.cs

--- src/Atlas.API/Extensions/Origin/AtlasOriginEndpointMapper.cs
+++ src/Atlas.API/Extensions/Origin/AtlasOriginEndpointMapper.cs
@@ -1,6 +1,7 @@
 ﻿using Atlas.API.Endpoints.Origin;
 using Atlas.Core.Constants;
 using Atlas.Core.Models;
+using Origin.Core.Constants;
 using Origin.Core.Models;
 
 namespace Atlas.API.Extensions.Origin
@@ -9,97 +10,97 @@ namespace Atlas.API.Extensions.Origin
     {
         internal static WebApplication? MapAtlasOriginEndpoints(this WebApplication app)
         {
-            app.MapGet($"/{AtlasAPIEndpoints.GET_DOCUMENT_CONFIGS}", DocumentEndpoints.GetDocumentConfigs)
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_CONFIGS}", DocumentEndpoints.GetDocumentConfigs)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.GET_DOCUMENT_CONFIGS)
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_CONFIGS)
                 .WithDescription("Gets a list of document configurations.")
                 .Produces<IEnumerable<DocumentConfig>?>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapGet($"/{AtlasAPIEndpoints.GET_DOCUMENT_CONFIG}/{{id:int}}", DocumentEndpoints.GetDocumentConfig)
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_CONFIG}/{{id:int}}", DocumentEndpoints.GetDocumentConfig)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.GET_DOCUMENT_CONFIG)
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_CONFIG)
                 .WithDescription("Gets a document configuration for the given id. If id is 0 then returns a new instance of a blank document configuration for creation.")
                 .Produces<DocumentConfig>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapPost($"/{AtlasAPIEndpoints.CREATE_DOCUMENT_CONFIG}", DocumentEndpoints.CreateDocumentConfig)
+            app.MapPost($"/{OriginAPIEndpoints.CREATE_DOCUMENT_CONFIG}", DocumentEndpoints.CreateDocumentConfig)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.CREATE_DOCUMENT_CONFIG)
+                .WithName(OriginAPIEndpoints.CREATE_DOCUMENT_CONFIG)
                 .WithDescription("Create a new document configuration.")
                 .Produces<DocumentConfig>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapPut($"/{AtlasAPIEndpoints.UPDATE_DOCUMENT_CONFIG}", DocumentEndpoints.UpdateDocumentConfig)
+            app.MapPut($"/{OriginAPIEndpoints.UPDATE_DOCUMENT_CONFIG}", DocumentEndpoints.UpdateDocumentConfig)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.UPDATE_DOCUMENT_CONFIG)
+                .WithName(OriginAPIEndpoints.UPDATE_DOCUMENT_CONFIG)
                 .WithDescription("Updates the document configuration.")
                 .Produces<DocumentConfig>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapDelete($"/{AtlasAPIEndpoints.DELETE_DOCUMENT_CONFIG}/{{id:int}}", DocumentEndpoints.DeleteDocumentConfig)
+            app.MapDelete($"/{OriginAPIEndpoints.DELETE_DOCUMENT_CONFIG}/{{id:int}}", DocumentEndpoints.DeleteDocumentConfig)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.DELETE_DOCUMENT_CONFIG)
+                .WithName(OriginAPIEndpoints.DELETE_DOCUMENT_CONFIG)
                 .WithDescription("Delete's a document configuration of the given id and returns the number of affected rows.")
                 .Produces<int>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapGet($"/{AtlasAPIEndpoints.GET_DOCUMENT_PARAGRAPHS}", DocumentEndpoints.GetDocumentParagraphs)
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_PARAGRAPHS}", DocumentEndpoints.GetDocumentParagraphs)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.GET_DOCUMENT_PARAGRAPHS)
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_PARAGRAPHS)
                 .WithDescription("Gets a list of document paragraphs.")
                 .Produces<IEnumerable<DocumentParagraph>?>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapGet($"/{AtlasAPIEndpoints.GET_DOCUMENT_PARAGRAPH}/{{id:int}}", DocumentEndpoints.GetDocumentParagraph)
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_PARAGRAPH}/{{id:int}}", DocumentEndpoints.GetDocumentParagraph)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.GET_DOCUMENT_PARAGRAPH)
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_PARAGRAPH)
                 .WithDescription("Gets a document paragraph for the given id. If id is 0 then returns a new instance of a blank document paragraph for creation.")
                 .Produces<DocumentParagraph>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapPost($"/{AtlasAPIEndpoints.CREATE_DOCUMENT_PARAGRAPH}", DocumentEndpoints.CreateDocumentParagraph)
+            app.MapPost($"/{OriginAPIEndpoints.CREATE_DOCUMENT_PARAGRAPH}", DocumentEndpoints.CreateDocumentParagraph)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.CREATE_DOCUMENT_PARAGRAPH)
+                .WithName(OriginAPIEndpoints.CREATE_DOCUMENT_PARAGRAPH)
                 .WithDescription("Create a new document paragraph.")
                 .Produces<DocumentParagraph>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapPut($"/{AtlasAPIEndpoints.UPDATE_DOCUMENT_PARAGRAPH}", DocumentEndpoints.UpdateDocumentParagraph)
+            app.MapPut($"/{OriginAPIEndpoints.UPDATE_DOCUMENT_PARAGRAPH}", DocumentEndpoints.UpdateDocumentParagraph)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.UPDATE_DOCUMENT_PARAGRAPH)
+                .WithName(OriginAPIEndpoints.UPDATE_DOCUMENT_PARAGRAPH)
                 .WithDescription("Updates the document paragraph.")
                 .Produces<DocumentParagraph>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapDelete($"/{AtlasAPIEndpoints.DELETE_DOCUMENT_PARAGRAPH}/{{id:int}}", DocumentEndpoints.DeleteDocumentParagraph)
+            app.MapDelete($"/{OriginAPIEndpoints.DELETE_DOCUMENT_PARAGRAPH}/{{id:int}}", DocumentEndpoints.DeleteDocumentParagraph)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.DELETE_DOCUMENT_PARAGRAPH)
+                .WithName(OriginAPIEndpoints.DELETE_DOCUMENT_PARAGRAPH)
                 .WithDescription("Delete's a document paragraph of the given id and returns the number of affected rows.")
                 .Produces<int>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapPost($"/{AtlasAPIEndpoints.GET_ORIGIN_OPTIONS}", OriginOptionsEndpoints.GetOriginOptions)
+            app.MapPost($"/{OriginAPIEndpoints.GET_ORIGIN_OPTIONS}", OriginOptionsEndpoints.GetOriginOptions)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.GET_ORIGIN_OPTIONS)
+                .WithName(OriginAPIEndpoints.GET_ORIGIN_OPTIONS)
                 .WithDescription("Gets origin option items for the specified origin options code")
                 .Produces<IEnumerable<OptionItem>?>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
-            app.MapPost($"/{AtlasAPIEndpoints.GET_GENERIC_ORIGIN_OPTIONS}", OriginOptionsEndpoints.GetGenericOriginOptions)
+            app.MapPost($"/{OriginAPIEndpoints.GET_GENERIC_ORIGIN_OPTIONS}", OriginOptionsEndpoints.GetGenericOriginOptions)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.GET_GENERIC_ORIGIN_OPTIONS)
+                .WithName(OriginAPIEndpoints.GET_GENERIC_ORIGIN_OPTIONS)
                 .WithDescription("Gets serialized generic origin options list for the specified origin options code")
                 .Produces<string>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
