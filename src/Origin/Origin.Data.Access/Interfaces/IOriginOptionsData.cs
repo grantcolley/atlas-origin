2623713f68eb14a3f366ff721ca582ba9af07b68
Grant Colley <grantcolley@yahoo.com>
2024-09-09 17:34:11 +0100

    Create IOriginOptionsData.cs

---
+++ src/Origin/Origin.Data.Access/Interfaces/IOriginOptionsData.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Core.Models;
+using Atlas.Data.Context;
+
+namespace Origin.Data.Access.Interfaces
+{
+    public interface IOriginOptionsData : IAuthorisationData
+    {
+        Task<IEnumerable<OptionItem>> GetOptionsAsync(IEnumerable<OptionsArg> optionsArgs, CancellationToken cancellationToken);
+        Task<string> GetGenericOptionsAsync(IEnumerable<OptionsArg> optionsArgs, CancellationToken cancellationToken);
+    }
+}
