96f3cc0ca23f7369bbb3f37861e5da5a6b79f5a3
Grant Colley <grantcolley@yahoo.com>
2024-09-09 20:23:31 +0100

    Update AtlasAPIEndpoints.cs

--- src/Atlas.Core/Constants/AtlasAPIEndpoints.cs
+++ src/Atlas.Core/Constants/AtlasAPIEndpoints.cs
@@ -39,18 +39,5 @@
         public const string DELETE_PERMISSION = "deletepermission";
         public const string GET_PERMISSION_CHECKLIST = "getpermissionchecklist";
         public const string GET_ROLE_CHECKLIST = "getrolechecklist";
-
-        public const string GET_DOCUMENT_CONFIG = "getdocumentconfig";
-        public const string GET_DOCUMENT_CONFIGS = "getdocumentconfigs";
-        public const string CREATE_DOCUMENT_CONFIG = "createdocumentconfig";
-        public const string UPDATE_DOCUMENT_CONFIG = "updatedocumentconfig";
-        public const string DELETE_DOCUMENT_CONFIG = "deletedocumentconfig";
-        public const string GET_DOCUMENT_PARAGRAPH = "getdocumentparagraph";
-        public const string GET_DOCUMENT_PARAGRAPHS = "getdocumentparagraphs";
-        public const string CREATE_DOCUMENT_PARAGRAPH = "createdocumentparagraph";
-        public const string UPDATE_DOCUMENT_PARAGRAPH = "updatedocumentparagraph";
-        public const string DELETE_DOCUMENT_PARAGRAPH = "deletedocumentparagraph";
-        public const string GET_ORIGIN_OPTIONS = "getoriginoptions";
-        public const string GET_GENERIC_ORIGIN_OPTIONS = "getgenericoriginoptions";
     }
 }
