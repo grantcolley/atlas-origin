8dc8a3ef2cf65c9b911e7d39a0e790b082aa3a4a
Grant Colley <grantcolley@yahoo.com>
2024-09-09 20:23:36 +0100

    Create OriginAPIEndpoints.cs

---
+++ src/Origin/Origin.Core/Constants/OriginAPIEndpoints.cs
@@ -0,0 +1,19 @@
+﻿namespace Origin.Core.Constants
+{
+    public static class OriginAPIEndpoints
+    {
+        public const string GET_DOCUMENT_CONFIG = "getdocumentconfig";
+        public const string GET_DOCUMENT_CONFIGS = "getdocumentconfigs";
+        public const string CREATE_DOCUMENT_CONFIG = "createdocumentconfig";
+        public const string UPDATE_DOCUMENT_CONFIG = "updatedocumentconfig";
+        public const string DELETE_DOCUMENT_CONFIG = "deletedocumentconfig";
+        public const string GET_DOCUMENT_PARAGRAPH = "getdocumentparagraph";
+        public const string GET_DOCUMENT_PARAGRAPHS = "getdocumentparagraphs";
+        public const string CREATE_DOCUMENT_PARAGRAPH = "createdocumentparagraph";
+        public const string UPDATE_DOCUMENT_PARAGRAPH = "updatedocumentparagraph";
+        public const string DELETE_DOCUMENT_PARAGRAPH = "deletedocumentparagraph";
+        public const string GET_ORIGIN_OPTIONS = "getoriginoptions";
+        public const string GET_GENERIC_ORIGIN_OPTIONS = "getgenericoriginoptions";
+    }
+}
+
