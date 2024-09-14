9309d28a561c5095f732cfc7e567f3bb54901f57
Grant Colley <grantcolley@yahoo.com>
2024-09-14 21:56:36 +0100

    Create DocumentColour.cs

---
+++ src/Origin/Origin.Core/Models/DocumentColour.cs
@@ -0,0 +1,26 @@
+﻿using Atlas.Core.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Text.Json.Serialization;
+
+namespace Origin.Core.Models
+{
+    public class DocumentColour : ModelBase
+    {
+        public int DocumentColourId { get; set; }
+
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// RGB 0-255 e.g. 0,176,240
+        /// </summary>
+        [StringLength(11)]
+        public string? Rgb { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public Color Color { get; set; }
+    }
+}
