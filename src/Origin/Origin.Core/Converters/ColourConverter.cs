9908d55cca3280354e3c9d370a680ab51b3010ff
Grant Colley <grantcolley@yahoo.com>
2024-09-14 21:57:05 +0100

    Update ColourConverter.cs

--- src/Origin/Origin.Core/Converters/ColourConverter.cs
+++ src/Origin/Origin.Core/Converters/ColourConverter.cs
@@ -1,14 +1,19 @@
-﻿namespace Origin.Core.Converters
+﻿using System.Drawing;
+
+namespace Origin.Core.Converters
 {
     public static class ColourConverter
     {
+        public static Color RgbToColor(this string value)
+        {
+            int[] rgb = value.SplitRgb();
+
+            return Color.FromArgb(rgb[0], rgb[1], rgb[2]);
+        }
+
         public static string RgbToHex(this string value)
         {
-            ArgumentException.ThrowIfNullOrWhiteSpace(value);
-
-            int[] rgb = value.Split(',').Select(int.Parse).ToArray();
-
-            if (rgb.Length != 3) throw new InvalidCastException(nameof(rgb));
+            int[] rgb = value.SplitRgb();
 
             rgb[0] = Math.Max(0, Math.Min(255, rgb[0]));
             rgb[1] = Math.Max(0, Math.Min(255, rgb[1]));
@@ -19,11 +24,7 @@
 
         public static byte[] RgbToByteArray(this string value)
         {
-            ArgumentException.ThrowIfNullOrWhiteSpace(value);
-
-            int[] rgb = value.Split(',').Select(int.Parse).ToArray();
-
-            if (rgb.Length != 3) throw new InvalidCastException(nameof(rgb));
+            int[] rgb = value.SplitRgb();
 
             byte[] rgbBytes =
             [
@@ -33,6 +34,17 @@
             ];
 
             return rgbBytes;
+        }
+
+        private static int[] SplitRgb(this string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+
+            int[] rgb = value.Split(',').Select(int.Parse).ToArray();
+
+            if (rgb.Length != 3) throw new InvalidCastException(nameof(rgb));
+
+            return rgb;
         }
     }
 }
