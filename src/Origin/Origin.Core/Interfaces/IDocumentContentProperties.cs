a675cdf29b583be7f76c7e47cfc20095a9b3b367
Grant Colley <grantcolley@yahoo.com>
2024-09-16 22:51:18 +0100

    Update IDocumentContentProperties.cs

--- src/Origin/Origin.Core/Interfaces/IDocumentContentProperties.cs
+++ src/Origin/Origin.Core/Interfaces/IDocumentContentProperties.cs
@@ -1,9 +1,13 @@
-﻿namespace Origin.Core.Interfaces
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Origin.Core.Interfaces
 {
     public interface IDocumentContentProperties
     {
         string? Font { get; set; }
         int? FontSize { get; set; }
+        string? SubstituteStart { get; set; }
+        string? SubstituteEnd { get; set; }
 
         /// <summary>
         /// RGB 0-255 e.g. 0,176,240
