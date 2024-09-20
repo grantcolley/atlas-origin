0105a016ab2e5b010943f5313db66c0d1ed05b90
Grant Colley <grantcolley@yahoo.com>
2024-09-20 20:47:13 +0100

    Create IDocumentPropertiesData.cs

---
+++ src/Origin/Origin.Data.Access/Interfaces/IDocumentPropertiesData.cs
@@ -0,0 +1,19 @@
+﻿using Atlas.Data.Context;
+using Origin.Core.Models;
+
+namespace Origin.Data.Access.Interfaces
+{
+    public interface IDocumentPropertiesData : IAuthorisationData
+    {
+        Task<IEnumerable<DocumentFont>> GetDocumentFontsAsync(CancellationToken cancellationToken);
+        Task<DocumentFont?> GetDocumentFontAsync(int id, CancellationToken cancellationToken);
+        Task<DocumentFont> CreateDocumentFontAsync(DocumentFont documentFont, CancellationToken cancellationToken);
+        Task<DocumentFont> UpdateDocumentFontAsync(DocumentFont documentFont, CancellationToken cancellationToken);
+        Task<int> DeleteDocumentFontAsync(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<DocumentColour>> GetDocumentColoursAsync(CancellationToken cancellationToken);
+        Task<DocumentColour?> GetDocumentColourAsync(int id, CancellationToken cancellationToken);
+        Task<DocumentColour> CreateDocumentColourAsync(DocumentColour documentColour, CancellationToken cancellationToken);
+        Task<DocumentColour> UpdateDocumentColourAsync(DocumentColour documentColour, CancellationToken cancellationToken);
+        Task<int> DeleteDocumentColourAsync(int id, CancellationToken cancellationToken);
+    }
+}
