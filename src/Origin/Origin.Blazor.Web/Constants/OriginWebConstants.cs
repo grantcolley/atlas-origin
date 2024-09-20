7e27efec9dd857c6c7cd58a67054d85fa1b8d830
Grant Colley <grantcolley@yahoo.com>
2024-09-20 20:47:24 +0100

    Update OriginWebConstants.cs

--- src/Origin/Origin.Blazor.Web/Constants/OriginWebConstants.cs
+++ src/Origin/Origin.Blazor.Web/Constants/OriginWebConstants.cs
@@ -6,5 +6,11 @@
         public const string PAGE_DOCUMENT_CONFIGS = "DocumentConfigs";
         public const string PAGE_DOCUMENT_PARAGRAPH = "DocumentParagraph";
         public const string PAGE_DOCUMENT_PARAGRAPHS = "DocumentParagraphs";
+
+
+        public const string PAGE_DOCUMENT_FONT = "DocumentFont";
+        public const string PAGE_DOCUMENT_FONTS = "DocumentFonts";
+        public const string PAGE_DOCUMENT_COLOUR = "DocumentColour";
+        public const string PAGE_DOCUMENT_COLOURS = "DocumentColours";
     }
 }
