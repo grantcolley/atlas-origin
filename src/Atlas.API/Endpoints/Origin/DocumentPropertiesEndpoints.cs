1b86f4d576161d8dab6d34a4e0c12e6eca056209
Grant Colley <grantcolley@yahoo.com>
2024-09-20 20:47:31 +0100

    Create DocumentPropertiesEndpoints.cs

---
+++ src/Atlas.API/Endpoints/Origin/DocumentPropertiesEndpoints.cs
@@ -0,0 +1,294 @@
+﻿using Atlas.API.Interfaces;
+using Atlas.Core.Constants;
+using Atlas.Core.Exceptions;
+using Atlas.Core.Logging.Interfaces;
+using Atlas.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Origin.Core.Models;
+using Origin.Data.Access.Interfaces;
+
+namespace Atlas.API.Endpoints.Origin
+{
+    internal static class DocumentPropertiesEndpoints
+    {
+        internal static async Task<IResult> GetDocumentFonts(IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_READ))
+                {
+                    return Results.Unauthorized();
+                }
+
+                IEnumerable<DocumentFont>? documentFonts = await documentData.GetDocumentFontsAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(new AuthResult<IEnumerable<DocumentFont>?> { Authorisation = authorisation, Result = documentFonts });
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> GetDocumentFont(int id, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_READ))
+                {
+                    return Results.Unauthorized();
+                }
+
+                DocumentFont? documentFont = await documentData.GetDocumentFontAsync(id, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(documentFont);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> CreateDocumentFont([FromBody] DocumentFont documentFont, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_WRITE))
+                {
+                    return Results.Unauthorized();
+                }
+
+                DocumentFont? newdocumentFont = await documentData.CreateDocumentFontAsync(documentFont, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(newdocumentFont);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> UpdateDocumentFont([FromBody] DocumentFont documentFont, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_WRITE))
+                {
+                    return Results.Unauthorized();
+                }
+
+                DocumentFont? updatedDocumentFont = await documentData.UpdateDocumentFontAsync(documentFont, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(updatedDocumentFont);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> DeleteDocumentFont(int id, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_WRITE))
+                {
+                    return Results.Unauthorized();
+                }
+
+                int affectedRows = await documentData.DeleteDocumentFontAsync(id, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(affectedRows);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> GetDocumentColours(IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_READ))
+                {
+                    return Results.Unauthorized();
+                }
+
+                IEnumerable<DocumentColour>? documentColours = await documentData.GetDocumentColoursAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(new AuthResult<IEnumerable<DocumentColour>?> { Authorisation = authorisation, Result = documentColours });
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> GetDocumentColour(int id, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_READ))
+                {
+                    return Results.Unauthorized();
+                }
+
+                DocumentColour? documentColour = await documentData.GetDocumentColourAsync(id, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(documentColour);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> CreateDocumentColour([FromBody] DocumentColour documentColour, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_WRITE))
+                {
+                    return Results.Unauthorized();
+                }
+
+                DocumentColour? newDocumentColour = await documentData.CreateDocumentColourAsync(documentColour, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(newDocumentColour);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> UpdateDocumentColour([FromBody] DocumentColour documentColour, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_WRITE))
+                {
+                    return Results.Unauthorized();
+                }
+
+                DocumentColour? updatedDocumentColour = await documentData.UpdateDocumentColourAsync(documentColour, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(updatedDocumentColour);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> DeleteDocumentColour(int id, IDocumentData documentData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        {
+            Authorisation? authorisation = null;
+
+            try
+            {
+                authorisation = await documentData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.DOCUMENT_WRITE))
+                {
+                    return Results.Unauthorized();
+                }
+
+                int affectedRows = await documentData.DeleteDocumentColourAsync(id, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(affectedRows);
+            }
+            catch (AtlasException ex)
+            {
+                logService.Log(Core.Logging.Enums.LogLevel.Error, ex.Message, ex, authorisation?.User);
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
