10eb3b4094004e3efab16b94b7f7b9dafafe1cee
Grant Colley <grantcolley@yahoo.com>
2024-09-20 20:47:34 +0100

    Update AtlasOriginEndpointMapper.cs

--- src/Atlas.API/Extensions/Origin/AtlasOriginEndpointMapper.cs
+++ src/Atlas.API/Extensions/Origin/AtlasOriginEndpointMapper.cs
@@ -90,6 +90,86 @@ namespace Atlas.API.Extensions.Origin
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
 
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_FONTS}", DocumentPropertiesEndpoints.GetDocumentFonts)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_FONTS)
+                .WithDescription("Gets a list of document fonts.")
+                .Produces<IEnumerable<DocumentFont>?>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_FONT}/{{id:int}}", DocumentPropertiesEndpoints.GetDocumentFont)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_FONT)
+                .WithDescription("Gets a document font for the given id.")
+                .Produces<DocumentFont>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapPost($"/{OriginAPIEndpoints.CREATE_DOCUMENT_FONT}", DocumentPropertiesEndpoints.CreateDocumentFont)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.CREATE_DOCUMENT_FONT)
+                .WithDescription("Create a new document font.")
+                .Produces<DocumentFont>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapPut($"/{OriginAPIEndpoints.UPDATE_DOCUMENT_FONT}", DocumentPropertiesEndpoints.UpdateDocumentFont)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.UPDATE_DOCUMENT_FONT)
+                .WithDescription("Updates the document font.")
+                .Produces<DocumentFont>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapDelete($"/{OriginAPIEndpoints.DELETE_DOCUMENT_FONT}/{{id:int}}", DocumentPropertiesEndpoints.DeleteDocumentFont)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.DELETE_DOCUMENT_FONT)
+                .WithDescription("Delete's a document font of the given id and returns the number of affected rows.")
+                .Produces<int>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_COLOURS}", DocumentPropertiesEndpoints.GetDocumentColours)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_COLOURS)
+                .WithDescription("Gets a list of document colours.")
+                .Produces<IEnumerable<DocumentColour>?>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapGet($"/{OriginAPIEndpoints.GET_DOCUMENT_COLOUR}/{{id:int}}", DocumentPropertiesEndpoints.GetDocumentColour)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.GET_DOCUMENT_COLOUR)
+                .WithDescription("Gets a document colour for the given id.")
+                .Produces<DocumentColour>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapPost($"/{OriginAPIEndpoints.CREATE_DOCUMENT_COLOUR}", DocumentPropertiesEndpoints.CreateDocumentColour)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.CREATE_DOCUMENT_COLOUR)
+                .WithDescription("Create a new document colour.")
+                .Produces<DocumentColour>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapPut($"/{OriginAPIEndpoints.UPDATE_DOCUMENT_COLOUR}", DocumentPropertiesEndpoints.UpdateDocumentColour)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.UPDATE_DOCUMENT_COLOUR)
+                .WithDescription("Updates the document colour.")
+                .Produces<DocumentColour>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapDelete($"/{OriginAPIEndpoints.DELETE_DOCUMENT_COLOUR}/{{id:int}}", DocumentPropertiesEndpoints.DeleteDocumentColour)
+                .WithOpenApi()
+                .WithName(OriginAPIEndpoints.DELETE_DOCUMENT_COLOUR)
+                .WithDescription("Delete's a document colour of the given id and returns the number of affected rows.")
+                .Produces<int>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
             app.MapPost($"/{OriginAPIEndpoints.GET_ORIGIN_OPTIONS}", OriginOptionsEndpoints.GetOriginOptions)
                 .WithOpenApi()
                 .WithName(OriginAPIEndpoints.GET_ORIGIN_OPTIONS)
