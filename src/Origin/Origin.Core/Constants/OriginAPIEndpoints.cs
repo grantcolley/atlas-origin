767b260fe51e2103e0e6ef4da79fccdb10a57371
Grant Colley <grantcolley@yahoo.com>
2024-09-20 20:47:21 +0100

    Update OriginAPIEndpoints.cs

--- src/Origin/Origin.Core/Constants/OriginAPIEndpoints.cs
+++ src/Origin/Origin.Core/Constants/OriginAPIEndpoints.cs
@@ -12,6 +12,16 @@
         public const string CREATE_DOCUMENT_PARAGRAPH = "createdocumentparagraph";
         public const string UPDATE_DOCUMENT_PARAGRAPH = "updatedocumentparagraph";
         public const string DELETE_DOCUMENT_PARAGRAPH = "deletedocumentparagraph";
+        public const string GET_DOCUMENT_FONT = "getdocumentfont";
+        public const string GET_DOCUMENT_FONTS = "getdocumentfonts";
+        public const string CREATE_DOCUMENT_FONT = "createdocumentfont";
+        public const string UPDATE_DOCUMENT_FONT = "updatedocumentfont";
+        public const string DELETE_DOCUMENT_FONT = "deletedocumentfont";
+        public const string GET_DOCUMENT_COLOUR = "getdocumentcolour";
+        public const string GET_DOCUMENT_COLOURS = "getdocumentcolours";
+        public const string CREATE_DOCUMENT_COLOUR = "createdocumentcolour";
+        public const string UPDATE_DOCUMENT_COLOUR = "updatedocumentcolour";
+        public const string DELETE_DOCUMENT_COLOUR = "deletedocumentcolour";
         public const string GET_ORIGIN_OPTIONS = "getoriginoptions";
         public const string GET_GENERIC_ORIGIN_OPTIONS = "getgenericoriginoptions";
     }
