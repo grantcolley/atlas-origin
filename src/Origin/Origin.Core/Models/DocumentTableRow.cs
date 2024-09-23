e5460dc5a4d3300a3458c229f96939c02ae0d329
Grant Colley <grantcolley@yahoo.com>
2024-09-23 20:48:49 +0100

    Update DocumentTableRow.cs

--- src/Origin/Origin.Core/Models/DocumentTableRow.cs
+++ src/Origin/Origin.Core/Models/DocumentTableRow.cs
@@ -1,11 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Atlas.Core.Models;
 
 namespace Origin.Core.Models
 {
-    public class DocumentTableRow
+    public class DocumentTableRow : ModelBase
     {
         public int DocumentTableRowId { get; set; }
-        public int Position { get; set; }
+        public int Number { get; set; }
         public int? Height { get; set; }
     }
 }
