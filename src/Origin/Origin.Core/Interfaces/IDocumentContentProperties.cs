741e68d90b719a6b2f0e18efb3311674b42b48ac
Grant Colley <grantcolley@yahoo.com>
2024-09-30 21:09:51 +0100

    Update IDocumentContentProperties.cs

--- src/Origin/Origin.Core/Interfaces/IDocumentContentProperties.cs
+++ src/Origin/Origin.Core/Interfaces/IDocumentContentProperties.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Origin.Core.Models;
 
 namespace Origin.Core.Interfaces
 {
     public interface IDocumentContentProperties
     {
+        DocumentContentAlign AlignContent { get; set; }
+        bool? IgnoreParapgraphSpacing { get; set; }
+
         string? Font { get; set; }
         int? FontSize { get; set; }
         string? SubstituteStart { get; set; }
