4fc0589893462cab83f534caac3d71265c38a855
Grant Colley <grantcolley@yahoo.com>
2024-10-04 21:20:16 +0100

    Update IDocumentData.cs

--- src/Origin/Origin.Data.Access/Interfaces/IDocumentData.cs
+++ src/Origin/Origin.Data.Access/Interfaces/IDocumentData.cs
@@ -15,15 +15,5 @@ namespace Origin.Data.Access.Interfaces
         Task<DocumentParagraph> CreateDocumentParagraphAsync(DocumentParagraph documentParagraph, CancellationToken cancellationToken);
         Task<DocumentParagraph> UpdateDocumentParagraphAsync(DocumentParagraph documentParagraph, CancellationToken cancellationToken);
         Task<int> DeleteDocumentParagraphAsync(int id, CancellationToken cancellationToken);
-        Task<IEnumerable<DocumentFont>> GetDocumentFontsAsync(CancellationToken cancellationToken);
-        Task<DocumentFont?> GetDocumentFontAsync(int id, CancellationToken cancellationToken);
-        Task<DocumentFont> CreateDocumentFontAsync(DocumentFont documentFont, CancellationToken cancellationToken);
-        Task<DocumentFont> UpdateDocumentFontAsync(DocumentFont documentFont, CancellationToken cancellationToken);
-        Task<int> DeleteDocumentFontAsync(int id, CancellationToken cancellationToken);
-        Task<IEnumerable<DocumentColour>> GetDocumentColoursAsync(CancellationToken cancellationToken);
-        Task<DocumentColour?> GetDocumentColourAsync(int id, CancellationToken cancellationToken);
-        Task<DocumentColour> CreateDocumentColourAsync(DocumentColour documentColour, CancellationToken cancellationToken);
-        Task<DocumentColour> UpdateDocumentColourAsync(DocumentColour documentColour, CancellationToken cancellationToken);
-        Task<int> DeleteDocumentColourAsync(int id, CancellationToken cancellationToken);
     }
 }
