5531500dfa86262c115d209bcd44a15e6993efe8
Grant Colley <grantcolley@yahoo.com>
2024-10-10 22:04:39 +0100

    Create TableCellCodes.cs

---
+++ tests/Origin.Test.Data/TableCellCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace Origin.Test.Data
+{
+    public static class TableCellCodes
+    {
+        public const string COMPANY_LOGO = "COMPANY_LOGO";
+        public const string COMPANY_DETAILS = "COMPANY_DETAILS";
+        public const string CUSTOMER_DETAILS = "CUSTOMER_DETAILS";
+        public const string SIGNATURE_DETAILS = "SIGNATURE_DETAILS";
+    }
+}
