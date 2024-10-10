10be30837d2b8979d9eed1f86e184a5bf95f5ba3
Grant Colley <grantcolley@yahoo.com>
2024-10-10 22:04:36 +0100

    Create ParagraphNames.cs

---
+++ tests/Origin.Test.Data/ParagraphNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Origin.Test.Data
+{
+    public static class ParagraphNames
+    {
+        public const string LETTER_HEAD_TABLE = "LETTER_HEAD_TABLE";
+        public const string LETTER_TITLE_PARAGRAPH = "LETTER_TITLE_PARAGRAPH";
+        public const string FOOTER_PARAGRAPH = "FOOTER_PARAGRAPH";
+        public const string SIGNATURE_TABLE = "SIGNATURE_TABLE";
+    }
+}
