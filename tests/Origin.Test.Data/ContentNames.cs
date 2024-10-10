dcb9dd853cdd160fd25e563555553131a68b967d
Grant Colley <grantcolley@yahoo.com>
2024-10-10 22:04:33 +0100

    Create ContentNames.cs

---
+++ tests/Origin.Test.Data/ContentNames.cs
@@ -0,0 +1,26 @@
+﻿namespace Origin.Test.Data
+{
+    public static class ContentNames
+    {
+        public const string LOGO = "LOGO";
+        public const string COMPANY_NAME = "COMPANY_NAME";
+        public const string COMPANY_ADDRESS_1 = "COMPANY_ADDRESS_1";
+        public const string COMPANY_ADDRESS_2 = "COMPANY_ADDRESS_2";
+        public const string COMPANY_ADDRESS_3 = "COMPANY_ADDRESS_3";
+        public const string COMPANY_PHONE_NUMBER = "COMPANY_PHONE_NUMBER";
+        public const string COMPANY_EMAIL = "COMPANY_EMAIL";
+        public const string CUSTOMER_NAME = "CUSTOMER_NAME";
+        public const string CUSTOMER_ADDRESS_1 = "CUSTOMER_ADDRESS_1";
+        public const string CUSTOMER_ADDRESS_2 = "CUSTOMER_ADDRESS_2";
+        public const string CUSTOMER_ADDRESS_3 = "CUSTOMER_ADDRESS_3";
+        public const string CUSTOMER_ADDRESS_4 = "CUSTOMER_ADDRESS_4";
+        public const string CUSTOMER_ADDRESS_5 = "CUSTOMER_ADDRESS_5";
+        public const string CUSTOMER_ADDRESS_6 = "CUSTOMER_ADDRESS_6";
+        public const string CUSTOMER_ADDRESS_7 = "CUSTOMER_ADDRESS_7";
+        public const string LETTER_TITLE = "LETTER_TITLE";
+        public const string FOOTER_TEXT = "FOOTER_TEXT";
+        public const string SIGNATURE = "SIGNATURE";
+        public const string SIGNEE = "SIGNEE";
+        public const string SIGNEE_TITLE = "SIGNEE_TITLE";
+    }
+}
