f5ee2d7c6031af5bdb6f9a98dbe5832d0badc3ba
Grant Colley <grantcolley@yahoo.com>
2024-10-12 19:46:46 +0100

    Update DocumentSubstitute.cs

--- src/Origin/Origin.Core/Models/DocumentSubstitute.cs
+++ src/Origin/Origin.Core/Models/DocumentSubstitute.cs
@@ -1,4 +1,5 @@
 ﻿using Atlas.Core.Models;
+using FluentValidation;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,5 +18,18 @@ namespace Origin.Core.Models
 
         [NotMapped]
         public string? Value { get; set; }
+    }
+
+    public class DocumentSubstituteValidator : AbstractValidator<DocumentSubstitute>
+    {
+        public DocumentSubstituteValidator()
+        {
+            RuleFor(v => v.Key)
+                .NotEmpty().WithMessage("Key is required")
+                .Length(1, 100).WithMessage("Key cannot exceed 100 characters");
+
+            RuleFor(v => v.Group)
+                .MaximumLength(100).WithMessage("Group cannot exceed 100 characters");
+        }
     }
 }
