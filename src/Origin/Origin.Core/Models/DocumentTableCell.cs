6b9184e0c85c64e67d5bd06ab2371869903c3d37
Grant Colley <grantcolley@yahoo.com>
2024-10-12 20:04:43 +0100

    Update DocumentTableCell.cs

--- src/Origin/Origin.Core/Models/DocumentTableCell.cs
+++ src/Origin/Origin.Core/Models/DocumentTableCell.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FluentValidation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -18,15 +19,40 @@ namespace Origin.Core.Models
         public int? BorderTop { get; set; }
         public int? BorderRight { get; set; }
         public int? BorderBottom { get; set; }
-        public string? BorderLeftColour { get; set; }
-        public string? BorderTopColour { get; set; }
-        public string? BorderRightColour { get; set; }
-        public string? BorderBottomColour { get; set; }
-        public string? CellColour { get; set; }
 
         [Required]
         [StringLength(100)]
         public string? CellCode { get; set; }
+
+        /// <summary>
+        /// RGB 0-255 e.g. 0,176,240
+        /// </summary>
+        [StringLength(11)]
+        public string? BorderLeftColour { get; set; }
+
+        /// <summary>
+        /// RGB 0-255 e.g. 0,176,240
+        /// </summary>
+        [StringLength(11)]
+        public string? BorderTopColour { get; set; }
+
+        /// <summary>
+        /// RGB 0-255 e.g. 0,176,240
+        /// </summary>
+        [StringLength(11)]
+        public string? BorderRightColour { get; set; }
+
+        /// <summary>
+        /// RGB 0-255 e.g. 0,176,240
+        /// </summary>
+        [StringLength(11)]
+        public string? BorderBottomColour { get; set; }
+
+        /// <summary>
+        /// RGB 0-255 e.g. 0,176,240
+        /// </summary>
+        [StringLength(11)]
+        public string? CellColour { get; set; }
 
         [NotMapped]
         [JsonIgnore]
@@ -35,6 +61,33 @@ namespace Origin.Core.Models
         public override int? GetId()
         {
             return DocumentTableCellId;
+        }
+    }
+
+    public class DocumentTableCellValidator : AbstractValidator<DocumentTableCell>
+    {
+        public DocumentTableCellValidator()
+        {
+            RuleFor(v => v.CellCode)
+                .NotEmpty().WithMessage("Cell Code is required")
+                .Length(1, 100).WithMessage("Cell Code cannot exceed 100 characters");
+
+            RuleFor(v => v.BorderLeftColour)
+                .MaximumLength(11).WithMessage("Border Left Colour cannot exceed 11 characters and must be in the format: RGB 0-255 e.g. 0,176,240");
+
+            RuleFor(v => v.BorderTopColour)
+                .MaximumLength(11).WithMessage("Border Top Colour cannot exceed 11 characters and must be in the format: RGB 0-255 e.g. 0,176,240");
+
+            RuleFor(v => v.BorderRightColour)
+                .MaximumLength(11).WithMessage("Border Right Colour cannot exceed 11 characters and must be in the format: RGB 0-255 e.g. 0,176,240");
+
+            RuleFor(v => v.BorderBottomColour)
+                .MaximumLength(11).WithMessage("Border Bottom Colour cannot exceed 11 characters and must be in the format: RGB 0-255 e.g. 0,176,240");
+
+            RuleFor(v => v.CellColour)
+                .MaximumLength(11).WithMessage("Cell Colour cannot exceed 11 characters and must be in the format: RGB 0-255 e.g. 0,176,240");
+
+            Include(new DocumentContentPropertiesValidator());
         }
     }
 }
