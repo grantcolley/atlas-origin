661e6fc63c35a9758ffebcab500a0271269e3787
Grant Colley <grantcolley@yahoo.com>
2024-10-30 17:48:50 +0000

    Create ParagraphSelect.cs

---
+++ src/Origin/Origin.Blazor.Web/Models/ParagraphSelect.cs
@@ -0,0 +1,13 @@
+﻿using Origin.Core.Models;
+
+namespace Origin.Blazor.Web.Models
+{
+    public class ParagraphSelect
+    {
+        public bool IsSelected { get; set; }
+        public int DocumentParagraphId { get; set; }
+        public string? Name { get; set; }
+        public DocumentParagraphType DocumentParagraphType { get; set; }
+        public string? Content { get; set; }
+    }
+}
