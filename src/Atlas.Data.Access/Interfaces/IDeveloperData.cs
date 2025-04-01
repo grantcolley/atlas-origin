ae0f7cb8b01eaf98a988c838866c925c477b176a
Grant Colley <grantcolley@yahoo.com>
2025-04-01 18:12:51 +0100

    Update IDeveloperData.cs

--- src/Atlas.Data.Access/Interfaces/IDeveloperData.cs
+++ src/Atlas.Data.Access/Interfaces/IDeveloperData.cs
@@ -4,8 +4,9 @@ namespace Atlas.Data.Access.Interfaces
 {
     public interface IDeveloperData
     {
+        void MigrateDatabase();
         Task<DatabaseStatus?> GetDatabaseStatusAsync(string? user, CancellationToken cancellationToken);
         Task<DatabaseStatus?> CreateDatabaseAsync(string? user, CancellationToken cancellationToken);
-        Task<DatabaseStatus?> SeedDatabaseAsync(string? user, CancellationToken cancellationToken);
+        Task<DatabaseStatus?> SeedDatabaseAsync(string? user, CancellationToken cancellationToken);        
     }
 }
